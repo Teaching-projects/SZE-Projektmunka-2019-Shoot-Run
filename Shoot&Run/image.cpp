182caad70ebaf2518b1384713f72d7b0d6db7bf2
lebbenszki <ati703@gmail.com>
2019-04-03 18:15:19 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/Teaching-projects/SZE-Projektmunka-2019-Shoot-Run""
    This reverts commit 1242840a7ab661f550e1ce2b0d55c50e1a515b9f.

---
+++ Shoot&Run/image.cpp
@@ -0,0 +1,11 @@
+#include "image.h"
+
+string image::getPath() const
+{
+return path;
+}
+
+datetime image::getDate() const
+{
+    return date_;
+}
--- Shoot&Run/main.cpp
+++ Shoot&Run/main.cpp
@@ -4,14 +4,16 @@
 #include <QFile>
 #include <iostream>
 #include <iomanip>
-#include "track.h"
-#include "image.h"
+#include "event.h"
+#include <algorithm>
 
 int main()
 {
+    event esemeny("Marathon",2018,9,26);
     QString ido;
     std::string stdido;
-    Track t("XY versenyzo");
+    Track t("Kocsis");
+    Track t2("Attila");
     QFile file("track.gpx");
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
         qDebug() << "File open error:" << file.errorString();
@@ -39,21 +41,62 @@ int main()
             }
         }
     }
+    QFile file2("track2.gpx");
+    if (!file2.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << "File open error:" << file.errorString();
+        return 1;
+    }
+    QXmlStreamReader inputStreamm(&file2);
+    while (!inputStreamm.atEnd() && !inputStreamm.hasError())
+    {
+        inputStreamm.readNext();
+        if (inputStreamm.isStartElement()) {
+            QString name = inputStreamm.name().toString();
+            if (name == "wpt"){
+                t2.addCoord(inputStreamm.attributes().value("lon").toDouble(),inputStreamm.attributes().value("lat").toDouble());
+            }
+            if (name=="time"){
+                ido=inputStreamm.readElementText();
+                stdido=ido.toStdString();
+                unsigned int year=stoul(stdido.substr(0,4));
+                unsigned int month=stoul(stdido.substr(5,2));
+                unsigned int day=stoul(stdido.substr(8,2));
+                unsigned int hour=stoul(stdido.substr(11,2));
+                unsigned int minute=stoul(stdido.substr(14,2));
+                unsigned int second=stoul(stdido.substr(17,2));
+                t2.addDate(year,month,day,hour,minute,second);
+            }
+        }
+    }
+    image egy("kep1",2016,2,16,14,0,33,49.21126,11.4627);
+    image ketto("kep2",2016,2,16,14,0,36,49.2143,11.4612);
+    image harom("kep3",2016,2,16,14,0,30,49.2144,11.4613);
     std::string path;
     std::cin>>path;
     image a(path);
-    a.print();
+    a.getCoord().print();
+    esemeny.imageAdd(a);
+    esemeny.imageAdd(egy);
+    esemeny.imageAdd(ketto);
+    esemeny.imageAdd(harom);
+    cout<<endl<<endl;
+    esemeny.sortt();
+    esemeny.trackAdd(t);
+    esemeny.trackAdd(t2);
+    //a.print();
     cout<<endl;
-    unsigned int j=0;
-    for(auto i:t.getDates()){
-        if((i->durationinseconds()>a.getDate().durationinseconds()-7) && (i->durationinseconds()<a.getDate().durationinseconds()+7)){
-            if(t.getCoordinates()[j]->distance(a.getCoord())<50){
-                cout<<t.getName()<<" rajt van "<<a.getPath()<<" kepen ";
-                i->print();
+    for(unsigned int i=0, j=0; i<t.getDates().size()&&j<esemeny.getImages().size();){
+        if(esemeny.getImages()[j]->getDate().durationinseconds()<t.getDates()[i]->durationinseconds())
+            j++;
+        else if(esemeny.getImages()[j]->getDate().durationinseconds()>t.getDates()[i]->durationinseconds())
+            i++;
+        else if((t.getDates()[i]->durationinseconds()>esemeny.getImages()[j]->getDate().durationinseconds()-7)&&(t.getDates()[i]->durationinseconds()<esemeny.getImages()[j]->getDate().durationinseconds()+7)){
+            if(t.getCoordinates()[i]->distance(esemeny.getImages()[j]->getCoord())<50){
+                cout<<t.getName()<<" rajt van "<<esemeny.getImages()[j]->getPath()<<" kepen "<<t.getCoordinates()[i]->distance(esemeny.getImages()[j]->getCoord())<<" m"<<endl;
+                t.getDates()[i]->print();
+                j++;
             }
         }
-    j++;
     }
-
     return 0;
 }
